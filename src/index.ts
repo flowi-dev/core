a514ab412b5125d3a180f6ea388e2fa724846284
T3CH <jlp02.dk@gmail.com>
2023-06-05 18:49:58 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export class Block {
+export class Vector {
   constructor(public x: number, public y: number) {}
+}
+
+export class Block {
+  constructor(public position: Vector, public size: Vector) {}
 }
