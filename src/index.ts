d9819db87a00e1285c5ba19e14d2f1b41af5ea0f
T3CH <jlp02.dk@gmail.com>
2023-06-05 20:10:00 +0200

    Add coverage shields

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export class Vector {
-  constructor(public x: number, public y: number) {}
-}
-
-export class Block {
-  constructor(public position: Vector, public size: Vector) {}
+export function add(a: number, b: number) {
+  return a + b;
 }
---
+++ test/add.test.ts
@@ -0,0 +1,5 @@
+import { add } from "../src";
+
+test("Add function", () => {
+  expect(add(1, 2)).toBe(3);
+});
