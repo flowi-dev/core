59a2721b3cd4d01dafb4f5a2abf0e59436eb3d79
T3CH <jlp02.dk@gmail.com>
2023-06-06 23:53:41 +0200

    Remove pin class

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ import 'reflect-metadata';
 export {AnyType, ArrayType, ObjectType, PrimitiveType, BaseType as Type, UnionType} from './classes/Type';
 
 export {ANY, BOOLEAN, FALSE, INTEGER, NULL, NUMBER, STRING, TRUE} from './primitives';
-
-export {Pin} from './classes/Pin';
