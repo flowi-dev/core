0a124f65c8275d444703290b8b88f46d12c8cbab
T3CH <jlp02.dk@gmail.com>
2023-06-07 11:59:52 +0200

    Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,19 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'xo',
-	],
-	overrides: [
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
-	ignorePatterns: [
-		'test',
-		'lib',
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-	],
-	rules: {
-	},
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ["plugin:react/recommended", "xo", "plugin:storybook/recommended"],
+  overrides: [{
+    extends: ['xo-typescript'],
+    files: ['*.ts', '*.tsx']
+  }],
+  ignorePatterns: ['test', 'lib'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {}
 };
