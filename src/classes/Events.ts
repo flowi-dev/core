9aecd252ceb2969746bea8788898f45ce987b38c
T3CH <jlp02.dk@gmail.com>
2023-06-08 13:52:26 +0200

    Added events and docs

---
+++ src/classes/Events.ts
@@ -0,0 +1,107 @@
+
+/**
+ * A simple event emitter. Takes a generic type that defines the events that can be emitted.
+ *
+ * ```ts
+ * type MyEvents = {
+ * 	'event-1': [string, number];
+ *  'event-2': [boolean];
+ * };
+ *
+ * const events = new Events<MyEvents>();
+ * ```
+ */
+export class Events<T extends Record<string, any[] >> {
+	public listeners: {
+		[key in keyof T]: Array<(...args: T[key]) => void>;
+	};
+
+	constructor() {
+		this.listeners = {} as unknown as typeof this.listeners;
+	}
+
+	/**
+	 * Add a listener to an event.
+	 *
+	 * ```ts
+	 * const listener = (arg1: string, arg2: number) => {
+	 * 	console.log(arg1, arg2);
+	 * };
+	 *
+	 * events.on('event-1', listener);
+	 * ```
+	 */
+	public on<K extends keyof T>(event: K, listener: (...args: T[K]) => void) {
+		if (!this.listeners[event]) {
+			this.listeners[event] = [];
+		}
+
+		this.listeners[event].push(listener);
+	}
+
+	/**
+	 * Remove a listener from an event.
+	 *
+	 * ```ts
+	 * events.off('event-1', listener);
+	 */
+	public off<K extends keyof T>(event: K, listener: (...args: T[K]) => void) {
+		if (!this.listeners[event]) {
+			return;
+		}
+
+		this.listeners[event] = this.listeners[event].filter(l => l !== listener);
+	}
+
+	/**
+	 * Emit an event with the given arguments.
+	 *
+	 * ```ts
+	 * events.emit('event-1', 'hello', 1);
+	 * ```
+	 */
+	public emit<K extends keyof T>(event: K, ...args: T[K]) {
+		if (!this.listeners[event]) {
+			return;
+		}
+
+		this.listeners[event].forEach(listener => {
+			listener(...args);
+		});
+	}
+
+	/**
+	 * Add a listener to an event that will only be called once.
+	 *
+	 * ```ts
+	 * events.once('event-1', listener);
+	 *
+	 * events.emit('event-1', 'hello', 1); // listener is called
+	 * events.emit('event-1', 'hello', 1); // listener is not called
+	 * ```
+	 */
+	public once<K extends keyof T>(event: K, listener: (...args: T[K]) => void) {
+		const onceListener = (...args: T[K]) => {
+			this.off(event, onceListener);
+			listener(...args);
+		};
+
+		this.on(event, onceListener);
+	}
+
+	/**
+	 * Clear all listeners.
+	 * ```ts
+	 * events.emit('event-1', 'hello', 1); // listeners are called
+	 * events.clear();
+	 * events.emit('event-1', 'hello', 1); // listeners are not called
+	 * ```
+	 */
+	public clear() {
+		this.listeners = {} as unknown as typeof this.listeners;
+	}
+
+	public getListeners<K extends keyof T>(event: K) {
+		return this.listeners[event] ?? [];
+	}
+}
--- src/classes/block/Attribute.ts
+++ src/classes/block/Attribute.ts
@@ -1,69 +1,127 @@
-import {Serializable} from '../Serialization';
-import {type Block} from './Block';
+import {Events} from '../Events';
+import {Serializable} from '../Serialization/Serializable';
 import {type BaseType} from '../Types';
+import {type Block} from './Block';
 
+/**
+ * The direction of the attribute. Inputs sit on the left side of the block, and outputs sit on the right side of the block.
+ */
+export type AttributeDirection = 'input' | 'output';
 export type AttributeName = `${string}-attribute`;
-export type InferAttributeName<T extends Attribute<any>> = T extends Attribute<infer N> ? N : never;
-export type InferAttributeType<T extends DataAttribute<any, BaseType>> = T extends DataAttribute<any, infer T> ? T : never;
+export type InferAttributeName<T extends Attribute<any>> = T extends Attribute<
+infer N
+>
+	? N
+	: never;
+export type InferAttributeType<T extends DataAttribute<any, BaseType>> =
+  T extends DataAttribute<any, infer T> ? T : never;
 
-export type AttributeOptions = {
-	label: string;
-	hideLabel: true;
-	hidePin: true;
+export type AttributeOptions<
+	Io extends AttributeDirection = AttributeDirection,
+> = {
+	label?: string;
+	hideLabel?: true;
+	hidePin?: true;
+	direction: Io;
+};
+
+export type AttributeEvents = {
+	'change:label': [string];
+	'change:hideLabel': [boolean];
+	'change:hidePin': [boolean];
 };
 
 /**
  * An attribute is a property of a block.
  */
-export class Attribute<N extends AttributeName = AttributeName> extends Serializable {
+
+export class Attribute<
+	N extends AttributeName = AttributeName,
+	Io extends AttributeDirection = AttributeDirection,
+> extends Serializable {
 	/**
-	 * Just like the label on a block, this is what the user sees, this cannot be changed by the user though.
-	 */
-	public label: string;
+   * This is used to emit and listen to events.
+   */
+	public events: Events<AttributeEvents> = new Events<AttributeEvents>();
 
 	/**
-	 * Whether or not to hide the label of the attribute. Can be used for attributes that have custom controllers that display the label.
-	 */
+   * The direction of the attribute. This determines whether or not the attribute is an input or an output.
+   */
+	public readonly direction: Io;
+
+	/**
+   * Just like the label on a block, this is what the user sees, this cannot be changed by the user though.
+   */
+	public get label() {
+		return this._label;
+	}
+
+	public set label(label: string) {
+		this._label = label;
+		this.events.emit('change:label', label);
+	}
+
+	/**
+   * Whether or not to hide the label of the attribute. Can be used for attributes that have custom controllers that display the label.
+   */
 	public hideLabel: boolean;
 
 	/**
-	 * Whether or not to hide the pin of the attribute. Can be used when you only want to display the controller
-	 */
+   * Whether or not to hide the pin of the attribute. Can be used when you only want to display the controller
+   */
 	public hidePin: boolean;
 
+	/** @ignore */
+	protected _label: string;
+
 	/**
-	 * Stores a reference to the block that this attribute belongs to.
+	 * Get the block that this attribute belongs to.
 	 */
-	private _block?: Block;
-
-	constructor(public readonly name: N, options?: Partial<AttributeOptions>) {
-		super(name);
-		this.label = options?.label ?? name;
-
-		this.hideLabel = options?.hideLabel ?? false;
-		this.hidePin = options?.hidePin ?? false;
+	get parent() {
+		return this._block;
 	}
 
-	public setParent(block: Block) {
+	/**
+   * Stores a reference to the block that this attribute belongs to.
+   */
+	private _block?: Block;
+
+	constructor(public readonly name: N, options: AttributeOptions<Io>) {
+		super(name);
+		this._label = options.label ?? name;
+		this.direction = options.direction;
+
+		this.hideLabel = options.hideLabel ?? false;
+		this.hidePin = options.hidePin ?? false;
+	}
+
+	public setParent(block: Block | undefined) {
 		this._block = block;
 	}
 }
 
-export type DataAttributeOptions<T extends BaseType> = {
+export type DataAttributeOptions<
+	T extends BaseType,
+	Io extends AttributeDirection,
+> = {
 	datatype: T;
 	defaultValue?: any;
-} & Partial<AttributeOptions>;
+} & AttributeOptions<Io>;
 
 /**
  * An attribute that holds data.
  */
-export class DataAttribute<N extends AttributeName, T extends BaseType> extends Attribute<N> {
+export class DataAttribute<
+	N extends AttributeName,
+	T extends BaseType,
+	Io extends AttributeDirection = AttributeDirection,
+> extends Attribute<N> {
 	/**
-	 * The type of data that this attribute holds.
-	 */
+   * The type of data that this attribute holds.
+   */
 	public datatype: BaseType;
 
-	constructor(name: N, options: DataAttributeOptions<T>) {
+	constructor(name: N, options: DataAttributeOptions<T, Io>) {
 		super(name, options);
 		this.datatype = options.datatype;
 	}
@@ -74,41 +132,4 @@ export type ExecutionAttributeOptions = Partial<AttributeOptions>;
 /**
  * An attribute that executes code when its executed by another block.
  */
-export class ExecutionAttribute<N extends AttributeName> extends Attribute<N> {
-	private _handler: (data: any, context: any) => void = () => {
-		throw new Error('No handler defined');
-	};
-
-	/**
-	 * This is the code that will be executed when the attribute is executed.
-	 */
-	// eslint-disable-next-line @typescript-eslint/member-ordering
-	public onExecute(handler: typeof ExecutionAttribute.prototype['_handler']) {
-		this._handler = handler;
-	}
-
-	/**
-	 * This function is used by the engine to execute this attribute.
-	 */
-	// eslint-disable-next-line @typescript-eslint/member-ordering
-	public execute(...args: Parameters<typeof ExecutionAttribute.prototype['_handler']>) {
-		this._handler(...args);
-	}
-}
-
-export class GetterAttribute<N extends AttributeName, T extends BaseType> extends DataAttribute<N, T> {
-	private _getter: (data: any, context: any) => any = () => {
-		throw new Error('No getter defined');
-	};
-
-	// eslint-disable-next-line @typescript-eslint/member-ordering
-	public onGet(handler: typeof GetterAttribute.prototype['_getter']) {
-		this._getter = handler;
-	}
-
-	// eslint-disable-next-line @typescript-eslint/member-ordering
-	public get(...args: Parameters<typeof GetterAttribute.prototype['_getter']>) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-		return this._getter(...args);
-	}
-}
+export class ExecutionAttribute<N extends AttributeName> extends Attribute<N> {}
